2875965ac10ba342fb6a58ae42f5b5adac0895f2
Anna Boldyreva <ann.boldyreva@tinkoff.ru>
2024-03-11 11:08:49 +0700

    Created project

---
+++ ProgrammingSkills/ProgrammingSkills/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgrammingSkills
+//
+//  Created by Anna Boldyreva on 11.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
