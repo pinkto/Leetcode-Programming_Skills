ff12abab4d0817915526001f32c49843902502c3
Anna Boldyreva <ann.boldyreva@tinkoff.ru>
2024-03-27 18:02:50 +0700

    Added explanations for each problem

--- ProgrammingSkills/ProgrammingSkills/Problems/1768. Merge Strings Alternately.swift
+++ ProgrammingSkills/ProgrammingSkills/Problems/1768. Merge Strings Alternately.swift
@@ -5,31 +5,81 @@
 //  Created by Anna Boldyreva on 11.03.2024.
 //
 
+/*
+ Даны две строки word1 и word2. Объедините строки, добавляя буквы в чередующемся порядке, начиная с word1.
+ Если одна из строк длиннее другой, добавьте дополнительные буквы в конец объединенной строки.
+ Верните объединенную строку.
+
+ Пример 1:
+
+ Вход: word1 = "abc", word2 = "pqr"
+ Выходные данные: "apbqcr".
+ Пояснения: Объединенная строка будет объединена следующим образом:
+ слово1: a b c
+ слово2: p q r
+ объединено: a p b q c r
+ 
+
+ Пример 2:
+
+ Вход: word1 = "ab", word2 = "pqrs"
+ Выход: "apbqrs"
+ Пояснения: Обратите внимание, что поскольку слово2 длиннее, в конце добавляется "rs".
+ слово1: a b
+ слово2: p q r s
+ объединено: a p b q r s
+ 
+ 
+ Пример 3:
+
+ Вход: word1 = "abcd", word2 = "pq"
+ Выходные данные: "apbqcd"
+ Пояснения: Обратите внимание, что поскольку слово1 длиннее, в конце добавляется "cd".
+ слово1: a b c d
+ слово2: p q
+ объединено: a p b q c d
+
+ */
+
 class MergeStringsAlternately {
     func mergeAlternately(_ word1: String, _ word2: String) -> String {
+
+        // Создадим две переменных-счётчика для отслеживания индексов символов в словах word1 и word1
         var i = 0 , j = 0
+
+        // Создадим массив для храннения объединенных символов
         var resultArray: Array<Character> = []
 
+        // Преобразуем строки word1 и word2 в массивы символов array1 и array2 соответсвенно
         let array1 = Array(word1)
         let array2 = Array(word2)
 
+        // Пока i меньше длины word1, а j меньше длины word2
         while i < word1.count && j < word2.count {
+            // Поочередно добавляем символы из word1 и word2 в resultArray,
             resultArray.append(array1[i])
             resultArray.append(array2[j])
+
+            // увеличивая i и j после каждой операции добавления
             i += 1
             j += 1
         }
 
+        // Проверям, есть ли оставшиеся символы в word1 (i меньше длины word1?).
+        // Если да, то добавляем оставшиеся символы word1 в resultArray
         while i < word1.count {
             resultArray.append(array1[i])
             i += 1
         }
-
+        // Проверяем, есть ли оставшиеся символы в word2 (j меньше длины word2?). 
+        // Если да, то добавляем оставшиеся символы word2 в resultArray
         while j < word2.count {
             resultArray.append(array2[j])
             j += 1
         }
 
+        // Преобразуем resultArray обратно в строку с помощью String(resultArray)
+        // и возвращаем объединенную строку
         return String(resultArray)
     }
 }
--- ProgrammingSkills/ProgrammingSkills/Problems/28. Find the Index of the First Occurrence in a String.swift
+++ ProgrammingSkills/ProgrammingSkills/Problems/28. Find the Index of the First Occurrence in a String.swift
@@ -5,18 +5,46 @@
 //  Created by Anna Boldyreva on 11.03.2024.
 //
 
+/* Даны две строки needle и haystack, верните индекс первого вхождения needle в haystack или -1, если needle не является частью haystack.
+
+Пример 1:
+Вход: haystack = "sadbutsad", needle = "sad"
+Выходные данные: 0
+
+Пояснения: Слово "sad" встречается в индексах 0 и 6.
+Первое вхождение находится в индексе 0, поэтому мы возвращаем 0.
+
+
+Пример 2:
+Вход: haystack = "leetcode", needle = "leeto"
+Выходные данные: -1
+
+Пояснение: "leeto" не встречается в "leetcode", поэтому мы возвращаем -1.
+ */
+
 class FindTheIndexClass {
     func strStr(_ haystack: String, _ needle: String) -> Int {
-        guard !Array(haystack).isEmpty else { return 0 }
 
+        // Проверяем, что строка не пустая
+        guard !Array(haystack).isEmpty else { return -1}
+
+        // Заводим две переменные, в которых будут храниться массивы символов строк haystack и needle
         let array1 = Array(haystack)
         let array2 = Array(needle)
 
+        // Проверям длина haystack больше или равна needle. Эта проверка необходима для того, чтобы убедиться, что в haystack достаточно символов для нахождения needle
         if array1.count >= array2.count {
+
             for i in 0...(array1.count - array2.count) {
                 for j in 0...array2.count {
+
+                    // Проверяем равен ли символ с текущим индексом i + j в haystack символу с индексом j в needle
                     if array1[i + j] != array2[j] {
+
+                        // Если они не равны, выходим из внутреннего цикла, поскольку это указывает на то, что подстрока, начинающаяся с индекса i в haystack, не совпадает с needle
                         break
+
+                    // Если символы одинаковы для всех индексов needle, значит, needle найден в haystack, начиная с индекса i, поэтому возвращается i
                     } else if j == array2.count - 1 {
                         return i
                     }
@@ -24,6 +52,7 @@ class FindTheIndexClass {
             }
         }
 
+       // Если вложенный цикл завершается, не найдя совпадения, то возвращается -1, так как needle не найден в haystack
         return -1
     }
 }
